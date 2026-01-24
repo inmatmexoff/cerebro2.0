09ae35b73fa8037739cdc25c4c5b3bd0711a90de
García Bautista César Emiliano <garcia.bautista.cesaremiliano@gmail.com>
2026-01-24 23:03:52 +0000

    agrega el icono de las 3 lineas en la esquina superior izq y el btn de d

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,9 +1,20 @@
 'use client'
 
+import { Button } from "@/components/ui/button"
+import { Download, Menu } from "lucide-react"
+
 export function SiteHeader() {
   return (
-    <header className="flex justify-start items-center mb-8">
-      <h1 className="text-4xl font-bold text-gray-800">Dashboard</h1>
+    <header className="flex justify-between items-center mb-8">
+      <div className="flex items-center gap-x-4">
+        <Button variant="ghost" size="icon" className="h-12 w-12">
+            <Menu className="h-10 w-10 text-gray-600" />
+        </Button>
+        <h1 className="text-4xl font-bold text-gray-800">Dashboard</h1>
+      </div>
+      <Button variant="ghost" size="icon" className="h-12 w-12">
+        <Download className="h-10 w-10 text-gray-600" />
+      </Button>
     </header>
   )
 }
