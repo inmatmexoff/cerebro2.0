f53f24971034a15eb8f5fcb5aef16e5fd58ce6e1
García Bautista César Emiliano <garcia.bautista.cesaremiliano@gmail.com>
2026-01-24 23:16:19 +0000

    solo hiciste más grande el btn que contiene al icono pero no al icono en

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -8,12 +8,12 @@ export function SiteHeader() {
     <header className="flex justify-between items-center mb-8">
       <div className="flex items-center gap-x-4">
         <Button variant="ghost" size="icon" className="h-24 w-24 hover:bg-transparent">
-            <Menu className="h-20 w-20 text-gray-600" />
+            <Menu className="h-16 w-16 text-gray-600" />
         </Button>
         <h1 className="text-4xl font-bold text-gray-800">Dashboard</h1>
       </div>
-      <Button variant="ghost" size="icon" className="h-24 w-24">
-        <Download className="h-20 w-20 text-gray-600" />
+      <Button className="h-24 w-24 bg-[#BABE65] hover:bg-[#A9AD5A] rounded-2xl">
+        <Download className="h-16 w-16 text-gray-700" />
       </Button>
     </header>
   )
