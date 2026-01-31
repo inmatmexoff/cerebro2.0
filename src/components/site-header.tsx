83c5bf8be830c4c05e58d938d082d22598371ed7
García Bautista César Emiliano <garcia.bautista.cesaremiliano@gmail.com>
2026-01-31 22:34:03 +0000

    cuando recien entro a / se ve asi la  versión movil, checa eso porque pa

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -9,7 +9,7 @@ export function SiteHeader() {
     <header className="flex justify-between items-center mb-8">
       <div className="flex items-center gap-x-4">
         <SidebarTrigger className="bg-transparent text-foreground hover:bg-transparent h-12 w-12 [&_svg]:h-6 [&_svg]:w-6" />
-        <h1 className="text-4xl font-bold text-gray-800">Dashboard</h1>
+        <h1 className="text-3xl md:text-4xl font-bold text-gray-800">Dashboard</h1>
       </div>
       <Button>
         <Download className="mr-2 h-4 w-4" />
