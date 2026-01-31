1e55406a79866e6f6d080be20d106e699de8c4db
García Bautista César Emiliano <garcia.bautista.cesaremiliano@gmail.com>
2026-01-31 23:44:21 +0000

    quiero que en la version de compu todos este en la misma fila

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -444,7 +444,7 @@ export default function DashboardPage() {
                    </div>
                  </div>
             ) : (
-              <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
+              <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-5 gap-6">
                  <DashboardCard
                     className="col-span-2 md:col-span-1 lg:col-span-1"
                     title={
