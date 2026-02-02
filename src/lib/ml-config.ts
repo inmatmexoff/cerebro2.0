b14b5e17f40f56daa6144ebe7c93197fd1391c25
García Bautista César Emiliano <garcia.bautista.cesaremiliano@gmail.com>
2026-02-02 18:45:18 +0000

    en mi api sale esto en esta url https://auth.mercadolibre.com.mx/authori

--- src/lib/ml-config.ts
+++ src/lib/ml-config.ts
@@ -4,7 +4,7 @@
 
 // It is strongly recommended to move these to environment variables for production.
 // e.g., NEXT_PUBLIC_ML_APP_ID and ML_CLIENT_SECRET
-export const ML_APP_ID = process.env.NEXT_PUBLIC_ML_APP_ID || '915941616480976';
+export const ML_APP_ID = process.env.NEXT_PUBLIC_ML_APP_ID || '915941614680976';
 export const ML_CLIENT_SECRET = process.env.ML_CLIENT_SECRET || '2QqfRfJo4wmZU3rMSijD2DPkxjLt5N2O';
 
 // The path for the redirect URI. The full URL will be constructed dynamically
