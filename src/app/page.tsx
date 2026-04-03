1ce5493be35ea4f0ef406049583bcd71de1af7ee
García Bautista César Emiliano <garcia.bautista.cesaremiliano@gmail.com>
2026-04-03 22:38:13 +0000

    INMATMEX y TAL COMERCIALIZADORA tienen el mismo color, cambialos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -263,11 +263,12 @@ async function getEtiquetasPorEmpresa(filters?: { startDate?: Date | null, endDa
       }
 
       const colorMap: { [key: string]: string } = {
-        'MTM': '#1B5E20',        // Verde Oscuro
-        'HOGARDEN': '#90EE90',  // Verde Claro
-        'DO MESKA': '#9370DB',   // Morado
-        'TAL': '#4682B4',        // Azul
-        'PALO DE ROSA': '#FFB6C1' // Rosa
+        'MTM': '#1B5E20',                 // Verde Oscuro
+        'HOGARDEN': '#90EE90',           // Verde Claro
+        'DO MESKA': '#9370DB',            // Morado
+        'PALO DE ROSA': '#FFB6C1',        // Rosa
+        'INMATMEX': '#FFA500',             // Naranja
+        'TAL COMERCIALIZADORA': '#4682B4', // Azul
       };
   
       const result = Object.entries(counts)
